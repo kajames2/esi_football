da5f11f37ba886971bf166d2e0a7364631e61120
kurod103 <kurod103@mail.chapman.edu>
2020-12-16 06:26:11 -0800

    Convert Playbyplay
    returns yards and tackles for a given player over a given game/season
    
    Not sure if I should add anything further, but it shouldn't be difficult from here

---
+++ Convert PBP/Convert_PBP.py
@@ -0,0 +1,93 @@
+import pandas as pd
+
+plays= pd.read_csv('PBP.csv')
+
+def getSeason(dfObj, value):
+    ''' Get i positions of value in dataframe i.e. dfObj.'''
+    list_rows = list()
+    # Get bool dataframe with True at positions where the given value exists
+    result = dfObj.isin([value])
+    # Get list of columns that contains the value
+    seriesObj = result.any()
+    columnNames = list(seriesObj[seriesObj == True].index)
+    print(columnNames)
+    # Iterate over list of columns and fetch the rows ies where value exists
+    for col in columnNames:
+        rows = list(result[col][result[col] == True].index)
+        for row in rows:
+            list_rows.append((row))
+    # Return a list of tuples iicating the positions of value in the dataframe
+    return list_rows
+
+def getGame (dfObj, value, game):
+    
+    i=len(dfObj)/10
+    index=0
+    gameFound=False
+    gameStart=False
+    gameEnd=False
+    while not gameStart:
+        while not gameFound:
+            index+=int(i) #i must be an integer
+            
+            if index >= len(dfObj): #when i exceeds df length, cut iteration by 2
+                i=i/2
+                index=0 #return i to 0
+                if i<=0:
+                    return 0
+            elif dfObj['gid'][index]==game:
+                gameFound=True
+        index-=1
+        if index<0:
+            index+=1
+            gameStart=True
+        if dfObj['gid'][index]!=game:
+            index+=1
+            gameStart=True
+    gameStart=index
+    while not gameEnd:
+        index+=1
+        if index>len(dfObj):
+            index-=1
+            gameEnd=True         
+        if dfObj['gid'][index]!=game:
+            index-=1
+            gameEnd=True
+    gameEnd=index
+    
+    dfObj=dfObj[gameStart:gameEnd]
+    list_rows=list()
+    result = dfObj.isin([value])
+    # Get list of columns that contains the value
+    seriesObj = result.any()
+    columnNames = list(seriesObj[seriesObj == True].index)
+        
+    for col in columnNames:
+        rows = list(result[col][result[col] == True].index)
+        for row in rows:
+            list_rows.append((row))
+    # Return a list of tuples iicating the positions of value in the dataframe
+    return list_rows
+
+def getYards(dfObj,rows):
+    count= []
+    for row in rows:
+        if not pd.isna(dfObj['yds'][row]):
+            count.append(dfObj['yds'][row])
+    return count
+
+def getTackles (dfObj, rows, player):
+    count=[]
+    for row in rows:
+        if not pd.isna(dfObj['rtck1'][row]):
+            count.append(1)
+            if not pd.isna(dfObj['rtck2'][row]):
+                count[-1]=.5
+
+
+print(len(plays))
+#getGame(plays,1)
+
+list_rows=getGame(plays,'CG-0400',1)
+print(list_rows)
+print(sum(getYards(plays,list_rows,'yds')))
